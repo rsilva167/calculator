104bfe57a6b237839d758fe720275b78d00539dd
Renan Santos <dosantosrenansilva@gmail.com>
2022-09-03 09:25:12 -0300

    This program uses a calculator class to perform basic operations.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(4, 5, 9);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
